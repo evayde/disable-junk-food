beaad45d14516e68a27e3f71b55ba342386e63fd
Pushkar Adhikari <pushkaradhikari1991@gmail.com>
2020-11-28 14:26:08 +0100

    Normal template

--- Components/InputTemplate.js
+++ Components/InputTemplate.js
@@ -1,21 +1,49 @@
 import {html, render} from 'lit-html';
-let foodIngredient = [{
+let foodIngredients = [{
     title: "Rice", used: true},
     {title: "chicken", used: true},
     {title: "lentil", used: false}
 ];
+
+let handleAddfood = (e) => {
+    if (e.key !== 'Enter') return;
+    foodIngredients.push({
+        title: e.target.value,
+        completed: false
+    });
+    e.target.value = '';
+    update();
+}
+
+let addFood = () => html`
+    <input class="newfood" placeholder = "add new food" autofocus=" " @keyup="${handleAddfood}" />
+`;
+
+let viewFoods = (foodIngredient) => html`
+    <li>
+        <div class="view">
+            <input class="toggle" type="checkbox" />
+            <label>${foodIngredient.title}</label>
+        </div>
+    </li>    
+`;
+
+
 let app = () => html`
     <section class="disableJunkFoodapp">
         <header class"header">
             <h1>foods</h1>
+            ${addFood()}
         </header>
 
         <section class="main">
             <ul class="food-list">
+                ${foodIngredients.map((foodIngredient) => viewFoods(foodIngredient))}
             </ul>
         </section>
     </section>
 
 `;
 
-render(app(), document.body)
+let update = () => render(app(), document.body);
+update();
